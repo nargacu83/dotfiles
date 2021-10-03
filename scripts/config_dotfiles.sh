54d599f54b1435875df707234ae95defe6e58a4a
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2021-10-03 17:37:44 +0200

    Nouveau dossier dotfiles

--- scripts/config_dotfiles.sh
+++ scripts/config_dotfiles.sh
@@ -5,13 +5,23 @@
 # Sourcing path is relative to install.sh
 source ./globals.sh
 
+dotfiles_directory="${HOME}/.dotfiles"
+dotfiles_root_directory="/opt/dotfiles"
 
 function install_dotfiles () {
     sudo mkdir /usr/share/xsessions
     
-    cd stow_home && stow * || print_error "Unable to install dotfiles"
+    # move and install user of the dotfiles
+    mv stow_home "${dotfiles_directory}" && \
+        cd "${dotfiles_directory}" \
+        && stow * || print_error "Unable to install dotfiles"
+    
+    # move and install root part of the dotfiles
+    sudo git clone https://gitlab.com/dev.quentinfranchi/dotfiles "${dotfiles_root_directory}_temp" \
+        && mv "${dotfiles_root_directory}_temp/stow_root" "${dotfiles_root_directory}" \
+        && rm -rf "${dotfiles_root_directory}_temp" \
+        && cd "${dotfiles_root_directory}"
 
-    sudo git clone https://gitlab.com/dev.quentinfranchi/dotfiles "/opt/dotfiles" && cd "/opt/dotfiles/stow_root"
     for directory in $( ls -p | grep / ); do
         CONFLICTS=$(stow --no --verbose ${directory} 2>&1 | awk '/\* existing target is/ {print $NF}')
         for f in ${CONFLICTS[@]}; do
