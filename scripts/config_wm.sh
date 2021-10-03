c2a5aa3fe72ce76aac2b95666c76bdaccd076f00
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2021-10-03 18:42:10 +0200

    Correction dwm print_error

--- scripts/config_wm.sh
+++ scripts/config_wm.sh
@@ -17,7 +17,7 @@ dwmblocks
 function install_dwm () {
     for _pkg in $PKG_DWM; do
         git clone --recurse-submodules https://gitlab.com/dev.quentinfranchi/${_pkg} "${INSTALL_DIRECTORY}/${_pkg}" \
-            && cd "${INSTALL_DIRECTORY}/${_pkg}/${_pkg}" && suckupdate || ERROR "Unable to install ${_pkg}"
+            && cd "${INSTALL_DIRECTORY}/${_pkg}/${_pkg}" && suckupdate || print_error "Unable to install ${_pkg}"
     done
 }
 
