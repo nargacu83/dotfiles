1ef5f8a8b5fa5ca5e0b3b42ac68d605175872da9
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2022-07-03 15:44:27 +0200

    WIP

--- install.sh
+++ install.sh
@@ -13,9 +13,12 @@ NORMAL=$(tput sgr0)
 # Global variables
 CURRENT_DIRECTORY="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 [ "$INSTALL_DIRECTORY" = "" ] && INSTALL_DIRECTORY="${CURRENT_DIRECTORY}/install"
+[ "$CONFIG_DIRECTORY" = "" ] && CONFIG_DIRECTORY="${CURRENT_DIRECTORY}/config"
 [ "$DOTFILES_DIRECTORY" = "" ] && DOTFILES_DIRECTORY="${INSTALL_DIRECTORY}/dotfiles"
 [ "$LOGS_DIRECTORY" = "" ] && LOGS_DIRECTORY="${CURRENT_DIRECTORY}/logs"
 SCRIPTS_DIRECTORY="${CURRENT_DIRECTORY}/scripts"
+
+errors=()
 
 function print_message() {
     echo -e "${BOLD}${2} ==> ${NC} ${BOLD}${1}${NORMAL}"
@@ -38,26 +41,24 @@ function log() {
   logfile="install.${count}.log"
 
   # log file already exists
-  if [ -f "${LOGS_DIRECTORY}/${logfile}" ]; then
-    # get it's new possible name
-    while [ -f "${LOGS_DIRECTORY}/${logfile}" ]; do
-        count=$(($count + 1))
-        logfile="install.${count}.log"
-    done
+  # if [ -f "${LOGS_DIRECTORY}/${logfile}" ]; then
+  #   # get it's new possible name
+  #   while [ -f "${LOGS_DIRECTORY}/${logfile}" ]; do
+  #       count=$(($count + 1))
+  #       logfile="install.${count}.log"
+  #   done
 
-    # rename the existing file
-    mv "${LOGS_DIRECTORY}/install.0.log" "${LOGS_DIRECTORY}/install.${count}.log"
-    echo "${LOGS_DIRECTORY}/${logfile}"
+  #   # rename the existing file
+  #   mv "${LOGS_DIRECTORY}/install.0.log" "${LOGS_DIRECTORY}/install.${count}.log"
 
-    # reset the name
-    count=0
-    logfile="install.${count}.log"
-  fi
+  #   # reset the name
+  #   count=0
+  #   logfile="install.${count}.log"
+  # fi
 
   echo "${1}" >> "${LOGS_DIRECTORY}/${logfile}"
 }
 
-errors=()
 function pacman_install() {
     doas pacman -S --needed --noconfirm ${1} || print_message "Could not install package: ${1}"
 }
@@ -112,7 +113,87 @@ function check_privileges() {
     [[ ${?} -eq 1 ]] && print_error "Your root password is wrong"
 }
 
+function check_config() {
+  # Get all config files
+  configs=()
+  for file in "${CONFIG_DIRECTORY}"/*; do
+      configs+=($(basename $file))
+  done
+
+  # Check if there are config files
+  if [ ${#configs[@]} -eq 0 ]; then
+    print_error "No configs detected"
+    exit 1
+  fi
+
+  # Check if selected config was not given
+  if [ "$SELECTED_CONFIG" == "" ]; then
+    SELECTED_CONFIG="default"
+  fi
+
+  # Check if the given configuration file name exists
+  valid=0
+  for config in "${configs[@]}"; do
+    if [ "$SELECTED_CONFIG" == "$config" ]; then
+      valid=1
+    fi
+  done
+  if [ $valid -eq 0 ]; then
+    print_error "Configuration file \"$SELECTED_CONFIG\" not found"
+    exit 1
+  fi
+}
+
+usage()
+{
+    echo "Usage: $0 [args]"
+    echo "Args:"
+    echo "--config <config_name>"
+    echo "--no-flatpak"
+    echo "--no-aur"
+    echo "--no-multilib"
+    echo "--parallel"
+}
+
+args=()
+while [ "${1:-}" != "" ]; do
+  args+=("${1:-}")
+  shift
+done
+
+for i in "${!args[@]}"; do
+  arg=${args[$i]}
+  case $arg in
+    --config)
+      SELECTED_CONFIG=${args[$(($i + 1))]}
+      ;;
+    --no-flatpak)
+      INSTALL_FLATPAK=0
+      ;;
+    --no-aur)
+      INSTALL_AUR=0
+      ;;
+    --no-multilib)
+      ENABLE_MULTILIB=0
+      ;;
+    --parallel)
+      PARALLEL_DOWNLOADS=${args[$(($i + 1))]}
+      if ! [[ $PARALLEL_DOWNLOADS =~ '^[0-9]+$' ]] ; then
+        PARALLEL_DOWNLOADS=0
+        print_error "Invalid argument for PARALLEL_DOWNLOADS"
+        exit 1
+      fi
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
 # check_privileges
+
+check_config
 
 # enable_parallel_downloads
 
@@ -122,33 +203,12 @@ function check_privileges() {
 
 # init_install_directory
 
+# source "${SCRIPTS_DIRECTORY}/config_fstab"
+
 # for script in "${SCRIPTS[@]}"; do
 #     source "${SCRIPTS_DIRECTORY}/${script}"
 # done
 
-log "yo"
-
-# sh "scripts/config_apps.sh"
-
 # clear_pacman_cache
 
 # remove_install_directory
-
-while [ "${1:-}" != "" ]; do
-  case "$1" in
-    -i|--install)
-      config_install
-      ;;
-    --no-flatpak)
-      INSTALL_FLATPAK=0
-      ;;
-    --no-aur)
-      INSTALL_AUR=0
-      ;;
-    -*|--*)
-      echo "Unknown option $1"
-      exit 1
-      ;;
-  esac
-  shift
-done
--- scripts/config_aur.sh
+++ scripts/config_aur.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# Installs packages from the Arch User Repository
-
-# Sourcing path is relative to install.sh
-source ./globals.sh
-
-PKG_AUR_APPS="
-
-"
-
-PKG_AUR_FONTS="
-
-"
-
-PKG_AUR_STYLE="
-
-"
-
-PKG_AUR_PRINTER="
-epson-inkjet-printer-escpr
-"
-
 function _install_paru () {
     cd ${INSTALL_FOLDER}
     git clone https://aur.archlinux.org/paru.git
@@ -31,7 +10,3 @@ function _install_paru () {
 print_message "Configuring AUR related packages"
 
 _install_paru
-paru -S --noconfirm --noprovides --skipreview ${PKG_AUR_APPS}
-paru -S --noconfirm --noprovides --skipreview ${PKG_AUR_FONTS}
-paru -S --noconfirm --noprovides --skipreview ${PKG_AUR_STYLE}
-paru -S --noconfirm --noprovides --skipreview ${PKG_AUR_PRINTER}
