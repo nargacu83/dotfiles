18d194db9f9a151f85cd1186abd85f551f851049
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2023-05-17 01:31:23 +0200

    MAJ Config waybar & hyprland

--- stow_home/hyprland/.config/hypr/autostart.sh
+++ stow_home/hyprland/.config/hypr/autostart.sh
@@ -3,8 +3,11 @@ export XDG_CURRENT_DESKTOP=Hyprland
 export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP=Hyprland
 export QT_QPA_PLATFORM=wayland;xcb
-export QT_QPA_PLATFORMTHEME=qt5ct;qt6ct
+export QT_QPA_PLATFORMTHEME=qt5ct
 export _JAVA_AWT_WM_NONREPARENTING=1
+export GTK_IM_MODULE='fcitx'
+export QT_IM_MODULE='fcitx'
+export XMODIFIERS='@im=fcitx'
 
 #set background
 if [ -x "$(command -v hyprpaper)" ]; then
@@ -29,18 +32,24 @@ if [ -x "$(command -v nm-applet)" ]; then
   nm-applet --indicator &
 fi
 
-# # bluetooth
+#start polkit
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# # bluetooth TODO: Find alternative that support Wayland.
 # if [ -x "$(command -v blueman-applet)" ]; then
 #   blueman-applet &
 # fi
 
-if [ -x "$(command -v emacs)" ]; then
-  systemctl --user start emacs
+# multilingual inputs
+if [ -x "$(command -v fcitx5)" ]; then
+  fcitx5 -d &
 fi
 
-if [ -x "$(command -v easyeffects)" ]; then
-  systemctl --user start easyeffects
-fi
-
-#start polkit
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# Atomic
+sleep 1
+killall xdg-desktop-portal-hyprland
+killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal
+/usr/libexec/xdg-desktop-portal-hyprland &
+sleep 2
+/usr/lib/xdg-desktop-portal &
