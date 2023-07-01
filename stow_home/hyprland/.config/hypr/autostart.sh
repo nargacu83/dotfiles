9ac2fe23322e9ecdb6391e34aba89528787490d1
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2023-07-01 22:02:33 +0200

    hyprland

--- stow_home/hyprland/.config/hypr/autostart.sh
+++ stow_home/hyprland/.config/hypr/autostart.sh
@@ -9,9 +9,12 @@ export GTK_IM_MODULE='fcitx'
 export QT_IM_MODULE='fcitx'
 export XMODIFIERS='@im=fcitx'
 
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+
 # wallpaper
 if [ -x "$(command -v swww)" ]; then
   swww init &
+  wallpapers /mnt/PERSO/Nextcloud/Wallpapers/Current/
 fi
 
 #start notification daemon
@@ -39,6 +42,14 @@ fi
 # if [ -x "$(command -v blueman-applet)" ]; then
 #   blueman-applet &
 # fi
+#
+if [ -x "$(command -v emacs)" ]; then
+  systemctl --user is-active --quiet emacs || systemctl --user restart emacs
+fi
+
+if [ -x "$(command -v easyeffects)" ]; then
+  systemctl --user is-active --quiet easyeffects || systemctl --user restart easyeffects
+fi
 
 # multilingual inputs
 if [ -x "$(command -v fcitx5)" ]; then
@@ -47,7 +58,8 @@ fi
 
 # Atomic
 sleep 1
-killall xdg-desktop-portal-hyprland
+killall xdg-desktop-portal-gtk
+killall xdg-desktop-portal-kde
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 /usr/libexec/xdg-desktop-portal-hyprland &
