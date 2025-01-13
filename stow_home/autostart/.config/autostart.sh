ede42583490a6d9563ef5e64774ad6d6f9c081ad
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2025-01-13 02:49:12 +0100

    Removed useless configuration in autostart

--- stow_home/autostart/.config/autostart.sh
+++ stow_home/autostart/.config/autostart.sh
@@ -2,6 +2,7 @@
 
 resolution="2560x1080"
 refresh_rate="100"
+gtk_theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
 
 # launch the given program in the background only if an instance isn't already running
 function launch_once() {
@@ -11,14 +12,12 @@ function launch_once() {
 if [ "$XDG_SESSION_TYPE" == "x11" ]; then
   echo "Autostarting for X11"
 
-  export GTK_IM_MODULE="fcitx"
-  export QT_IM_MODULE="fcitx"
-  export XMODIFIERS="@im=fcitx"
-
   if [ "$XDG_CURRENT_DESKTOP" != "KDE" ]; then
     export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
     export _JAVA_AWT_WM_NONEREPARENTING=1
     export QT_QPA_PLATFORMTHEME=qt5ct
+    #export GTK_THEME="Catppuccin-Mocha-Standard-Mauve-Dark"
+    export GTK_THEME="${gtk_theme//\'/}"
 
     #set resolution and refresh rate
     if [ -x "$(command -v xrandr)" ]; then
@@ -40,23 +39,13 @@ if [ "$XDG_SESSION_TYPE" == "x11" ]; then
       nm-applet --indicator &
     fi
 
-    # multilingual inputs
-    if [ -x "$(command -v fcitx5)" ]; then
-      fcitx5 -d &
-    fi
-
     # set night light
     if [ -x "$(command -v gammastep-indicator)" ]; then
       launch_once gammastep-indicator
     fi
 
-    # sync
-    if [ -x "$(command -v syncthingtray)" ]; then
-      syncthingtray --wait --replace &
-    fi
-
     # Start polkit
-    /usr/lib/polkit-kde-authentication-agent-1 &
+    /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
     # Disable screen sleep
     xset s off
@@ -80,14 +69,6 @@ if [ "$XDG_SESSION_TYPE" == "x11" ]; then
   fi
 
 elif [ "$XDG_SESSION_TYPE" == "wayland" ]; then
-
-  # River specific
-  # if [ "$XDG_CURRENT_DESKTOP" == "river" ]; then
-  #   echo "Autostarting for river"
-
-  #   export XDG_SESSION_DESKTOP=river
-  # fi
-
   # Hyprland specific
   if [ "$XDG_CURRENT_DESKTOP" == "Hyprland" ]; then
     echo "Autostarting for Hyprland"
@@ -98,8 +79,6 @@ elif [ "$XDG_SESSION_TYPE" == "wayland" ]; then
   if [ "$XDG_CURRENT_DESKTOP" != "KDE" ]; then
     export XCURSOR_SIZE=22
     export GTK_IM_MODULE=wayland
-    export QT_IM_MODULE=fcitx
-    export XMODIFIERS="@im=fcitx"
 
     export QT_AUTO_SCREEN_SCALE_FACTOR=1
     export QT_QPA_PLATFORM=wayland;xcb
@@ -123,11 +102,6 @@ elif [ "$XDG_SESSION_TYPE" == "wayland" ]; then
       gammastep-indicator -m wayland &
     fi
 
-    # multilingual inputs
-    if [ -x "$(command -v fcitx5)" ]; then
-      fcitx5 -d &
-    fi
-
     # network manager applet
     if [ -x "$(command -v nm-applet)" ]; then
       nm-applet &
@@ -139,12 +113,7 @@ elif [ "$XDG_SESSION_TYPE" == "wayland" ]; then
     fi
 
     # Polkit
-    /usr/lib/polkit-kde-authentication-agent-1 &
-
-    # wallpaper
-    #if [ -x "$(command -v swww)" ]; then
-    #  wallpapers "${wallpapers_directory}" &
-    #fi
+    /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
   fi
 fi
 
@@ -152,8 +121,3 @@ fi
 if [ -x "$(command -v dunst)" ]; then
   dunst &
 fi
-# REPLACED BY SYSTEMD SERVICE
-# audio management
-# if [ -x "$(command -v easyeffects)" ]; then
-#   easyeffects --gapplication-service &
-# fi
