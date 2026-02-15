387786aba4f545c0d1e03475048979324fc2f1af
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2026-02-15 21:17:30 +0100

    Improved gpu info script

--- stow_home/waybar/.config/waybar/scripts/gpu.sh
+++ stow_home/waybar/.config/waybar/scripts/gpu.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
+gpu_identifier="hwmon5"
 
-used=$(cat /sys/class/hwmon/hwmon3/device/mem_info_vram_used)
-total=$(cat /sys/class/hwmon/hwmon3/device/mem_info_vram_total)
+used=$(cat /sys/class/hwmon/${gpu_identifier}/device/mem_info_vram_used)
+total=$(cat /sys/class/hwmon/${gpu_identifier}/device/mem_info_vram_total)
 # convert to MiB
 used=$((used / 1048576))
 total=$((total / 1048576))
-# percent=$(cat /sys/class/hwmon/hwmon3/device/gpu_busy_percent)
+# percent=$(cat /sys/class/hwmon/$gpu_identifier/device/gpu_busy_percent)
 # echo "$percent %"
 
 echo "$used / $total MiB"
